ef66fc9be03a9f316f6c4a581a38648610059ef7
Joan Marín <36265613+X0KA@users.noreply.github.com>
2019-04-12 01:45:24 +0200

    Deleted some useless declarations

--- Order_227/Motor2D/Unit.h
+++ Order_227/Motor2D/Unit.h
@@ -17,14 +17,7 @@ public:
 	Unit(fPoint pos, entity_type Entitytype, entity_faction faction = entity_faction::FACTION_NEUTRAL):Entity(pos, type, faction){}
 	~Unit();
 
-	virtual bool Update(float dt) { return true; }
 	virtual bool Move(float dt) { return true; }
-
-	virtual bool Draw(float dt) { return true; }
-
-public:
-
-	//virtual void UpdateBlitOrder() {}
 
 public:
 
