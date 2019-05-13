5a745545f26e8ca2176ce88bdb7ebf5199fcefba
Jaumeavinyo <36154250+Jaumeavinyo@users.noreply.github.com>
2019-05-13 17:57:18 +0200

    Revert "CHRONO UPDATED"
    This reverts commit 33950905c72aeced6c947108afff4247d7274a3a.

--- Order_227/Motor2D/Entity_Manager.cpp
+++ Order_227/Motor2D/Entity_Manager.cpp
@@ -382,11 +382,6 @@ bool Entity_Manager::loadTextures() {
 
 		switch (Data.attribute("id").as_int())
 		{
-		case(int(infantry_type::BASIC)):
-			infantryTextures[int(infantry_type::BASIC)] = myApp->tex->Load(Data.attribute("TextPath").as_string());
-
-			break;
-
 		case(int(infantry_type::CONSCRIPT)):
 			infantryTextures[int(infantry_type::CONSCRIPT)] = myApp->tex->Load(Data.attribute("TextPath").as_string());
 
@@ -403,9 +398,9 @@ bool Entity_Manager::loadTextures() {
 			infantryTextures[int(infantry_type::DESOLATOR)] = myApp->tex->Load(Data.attribute("TextPath").as_string());
 			break;
 
-		case(int(infantry_type::CHRONO)):
+		case(int(infantry_type::MACHINE_GUN)):
 
-			infantryTextures[int(infantry_type::CHRONO)] = myApp->tex->Load(Data.attribute("TextPath").as_string());
+			infantryTextures[int(infantry_type::MACHINE_GUN)] = myApp->tex->Load(Data.attribute("TextPath").as_string());
 
 
 			break;
@@ -522,7 +517,7 @@ bool Entity_Manager::AssignAnimData(std::string faction) {
 
 						break;
 
-					case (int(infantry_type::CHRONO)):
+					case (int(infantry_type::MACHINE_GUN)):
 
 						//Introduce offset
 						temp.x += DataXML.child("RectOffset").attribute("x").as_int();
